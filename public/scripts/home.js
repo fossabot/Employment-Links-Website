f85abae135e7b708db0153de65f505d3907384ea
CSmith <CSmith14@pm.me>
2019-05-04 12:33:42 -0500

    Updated dependencies to remove vulnerabilities

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -5,32 +5,17 @@
  *
  */
 function mobileAdjustment() {
-  const opp = document.querySelector( '.opportunities' ),
-    oppCardContainer = opp.children[ 0 ],
-    oppCardRow = oppCardContainer.children[ 1 ],
-    card0 = oppCardRow.children[ 0 ],
-    serviceCard = document.querySelector( '#primary' ),
-    serviceCardClone = serviceCard.cloneNode( -1 ),
-    careerCard = document.querySelector( '#secondary' ),
-    careerCardClone = careerCard.cloneNode( -1 ),
-    cardButtons = oppCardRow.getElementsByTagName( 'a' );
   if ( window.screen.width <= 768 ) {
-
-    oppCardRow.removeChild( serviceCard );
-    oppCardRow.removeChild( careerCard );
-    oppCardRow.prepend( careerCardClone );
-    oppCardRow.prepend( serviceCardClone );
+    document.querySelector( 'section' ).classList.remove( 'bg-image-fixed' );
+    document.querySelector( 'section' ).classList.add( 'bg-image' );
 
     document.querySelectorAll( '.display-switch' ).forEach( function ( el ) {
       el.classList.remove( 'display-1', 'display-2', 'display-3', 'display-4' )
     } );
-
-
   } else {
-    oppCardRow.removeChild( serviceCard );
-    oppCardRow.removeChild( careerCard );
-    oppCardRow.prepend( serviceCardClone );
-    oppCardRow.prepend( careerCardClone );
+    document.querySelector( 'section' ).classList.add( 'bg-image-fixed' );
+    document.querySelector( 'section' ).classList.remove( 'bg-image' );
+
     document.querySelectorAll( '.display-switch' ).forEach( function ( el ) {
       var classList = el.classList;
       if ( classList.contains( 'd1' ) ) classList.add( 'display-1' );
@@ -38,7 +23,6 @@ function mobileAdjustment() {
       if ( classList.contains( 'd3' ) ) classList.add( 'display-3' );
       if ( classList.contains( 'd4' ) ) classList.add( 'display-4' );
     } );
-
   }
 }
 
