e30261788d3266d32ced59f9ad8e7f320ba0941e
CSmith <CSmith14@pm.me>
2019-05-04 18:00:37 -0500

    Added quick contact modal views and function on home page

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -6,5 +6,3 @@ var spacer = document.createElement( 'div' );
 spacer.setAttribute( 'id', 'spacer' )
 spacer.setAttribute( 'style', 'height: '+headerHeight+'px;' );
 
-document.querySelector('section').prepend( spacer );
-
